a3cab22100ed1dcd1d42c2075b442565a8765012
Paul <javelinanddart@gmail.com>
2015-02-11 13:08:05 -0600

    Remove S2W cmdline interfacing, Useless since nothing else has it
    Signed-off-by: Paul <javelinanddart@gmail.com>

--- drivers/input/touchscreen/cy8c_cs.c
+++ drivers/input/touchscreen/cy8c_cs.c
@@ -107,24 +107,6 @@ cputime64_t dt2w_time[2] = {0, 0};
 static struct input_dev * sweep2wake_pwrdev;
 static DEFINE_MUTEX(pwrlock);
 
-#ifdef CONFIG_CMDLINE_OPTIONS
-static int __init cy8c_read_s2w_cmdline(char *s2w)
-{
-	if (strcmp(s2w, "1") == 0) {
-		printk(KERN_INFO "[cmdline_s2w]: Sweep2Wake enabled. | s2w='%s'", s2w);
-		s2w_switch = 1;
-	} else if (strcmp(s2w, "0") == 0) {
-		printk(KERN_INFO "[cmdline_s2w]: Sweep2Wake disabled. | s2w='%s'", s2w);
-		s2w_switch = 0;
-	} else {
-		printk(KERN_INFO "[cmdline_s2w]: No valid input found. Sweep2Wake disabled. | s2w='%s'", s2w);
-		s2w_switch = 0;
-	}
-	return 1;
-}
-__setup("s2w=", cy8c_read_s2w_cmdline);
-#endif
-
 extern void sweep2wake_setdev(struct input_dev * input_device) {
 	sweep2wake_pwrdev = input_device;
 	return;
